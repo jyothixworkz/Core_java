6cb433705b624ef244e800d15e947a26eef56967
Dell <manojgowdajayaramjyothi1998mail.com>
2022-11-09 01:25:16 +0530

    wrapperclass

---
+++ opps/src/com/xworkz/wraperclass/W.java
@@ -0,0 +1,43 @@
+package com.xworkz.wraperclass;
+
+public class W {
+
+	public static void main(String[] args) {
+		int m = 10;
+		String s = "127";
+		Byte valueOf3 = Byte.valueOf(s);
+		System.out.println(valueOf3);
+
+		Integer valueOf = Integer.valueOf(m);
+		Integer valueOf2 = Integer.valueOf(s);
+		System.out.println(valueOf);
+		System.out.println(valueOf2);
+		Integer valueOf4 = Integer.valueOf("1111", 2);
+		System.out.println(valueOf4);
+		// xxxvalue
+		Integer integer = new Integer(10);
+		int intValue = integer.intValue();
+		byte byteValue = integer.byteValue();
+		float floatValue = integer.floatValue();
+		System.out.println(floatValue);
+		Character character = new Character('a');
+		char charValue = character.charValue();
+		System.out.println(charValue);
+		// parsexxx()
+		
+		String m1 = "100";
+		byte parseByte = Byte.parseByte(m1);
+
+		System.out.println(parseByte);
+
+		// tostring
+
+		// int b=10;
+
+		Integer integer2 = new Integer(10);
+		String string = integer2.toString();
+		System.out.println(string);
+
+	}
+
+}
